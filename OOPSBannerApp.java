2da0219c5c246f52b0ad2818b2dff2a3541986c8
Shaik Younis <shaikyounis241107@gmail.com>
2026-03-01 21:26:18 +0530

    Refactor banner printing using an array and loop
    Refactored the main method to store banner lines in an array and print them using a loop.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,56 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-        
-}
+
+        // Store all banner lines inside an array
+        String[] banner = {
+
+                String.join(" ",
+                        "  ***  ",
+                        "  ***  ",
+                        "  ** ",
+                        "  ***  "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *       "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " **  ",
+                        "  ***  "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        "       * "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        " *     * "),
+
+                String.join(" ",
+                        "  ***  ",
+                        "  ***  ",
+                        " *       ",
+                        "  ***  ")
+        };
+
+        // Use loop instead of multiple print statements
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
