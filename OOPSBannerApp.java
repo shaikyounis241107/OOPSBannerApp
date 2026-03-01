2095623a3aa7c6fd478a2f555d86a5ee60f7f2ee
Shaik Younis <shaikyounis241107@gmail.com>
2026-03-01 21:15:40 +0530

    Refactor main method to display banner
    Updated the main method to print a banner pattern.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,10 @@
-public class OOPSBannerApp {
+public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        System.out.println("OOPS");
-        
-}
+    System.out.println("  *   *   *   *   *  *  *   *  *  ");
+    System.out.println(" *     * *     *  *     *  *      ");
+    System.out.println(" *     * *     *  *     *  *      ");
+    System.out.println(" *     * *     *  *  *  *   *  *  ");
+    System.out.println(" *     * *     *  *            *  ");
+    System.out.println(" *     * *     *  *            *  ");
+    System.out.println("  *   *    *  *   *        *  *   ");
 }
