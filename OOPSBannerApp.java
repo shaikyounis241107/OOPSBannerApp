7fa68bf4c66252f9fb410a63ebebe935b5260068
Shaik Younis <shaikyounis241107@gmail.com>
2026-03-01 21:29:33 +0530

    Refactor banner initialization and printing
    Refactor banner printing to use an inline array and enhanced for-loop.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,83 @@
+feature/UC5-InlineArray
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
+
+        // Inline Array Initialization with String.join()
+        String[] banner = {
+
+ feature/UC1-PrintOOPS
+
+
+public class OOPSBannerApp {
+
+  
+    public static void main(String[] args) {
+
+       
         System.out.println("OOPS");
-        
-}
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Store all banner lines inside an array
+        String[] banner = {
+
+            main
+                String.join(" ",
+                        "  ***  ",
+                        "  ***  ",
+                        "  ** ",
+                        "  ***  "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *       "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " **  ",
+                        "  ***  "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        "       * "),
+
+                String.join(" ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        " *     * "),
+
+                String.join(" ",
+                        "  ***  ",
+                        "  ***  ",
+                        " *       ",
+                        "  ***  ")
+        };
+
+         feature/UC5-InlineArray
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+
+        // Use loop instead of multiple print statements
+        for (String line : banner) {
+            System.out.println(line);
+        }
+        main
+    }
 }
